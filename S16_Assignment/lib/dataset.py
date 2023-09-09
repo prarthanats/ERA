5ee80190e23e7d44a83e22eb17a3a345e92ec194
PrarthanaTS <32382676+prarthanats@users.noreply.github.com>
2023-09-09 08:48:47 +0530

    Add files via upload

---
+++ S16_Assignment/lib/dataset.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 30 14:28:08 2023
+@author: prarthana.ts
+"""
+import torch
+from torch.utils.data import Dataset
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+class BilingualDataset(Dataset):
+    def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang):
+        super().__init__()
+        self.ds = ds
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+        self.tokenizer_src = tokenizer_src
+        self.tokenizer_tgt = tokenizer_tgt
+        self.sos_token = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64)
+        self.eos_token = torch.tensor([tokenizer_tgt.token_to_id("[EOS]")], dtype=torch.int64)
+        self.pad_token = torch.tensor([tokenizer_tgt.token_to_id("[PAD]")], dtype=torch.int64)
+
+
+    def __len__(self):
+        return(len(self.ds))
+
+    def __getitem__(self, idx):
+        src_target_pair = self.ds[idx]
+        src_text = src_target_pair['translation'][self.src_lang]
+        tgt_text = src_target_pair['translation'][self.tgt_lang]
+
+        # Transform the text into tokens
+        enc_input_tokens = self.tokenizer_src.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
+
+        # Add <s> and </s> token
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token
+            ],
+            dim=0
+        )
+
+        # Add only </s> token
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                self.eos_token
+            ],
+            dim=0
+        )
+
+        # Add only <s> token
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64)
+            ],
+            dim=0
+        )
+
+        return({
+            "encoder_input": encoder_input, # (seq_len)
+            "decoder_input": decoder_input, # (seq_len)
+            "label": label, # (seq_len)
+            "src_text": src_text,
+            "tgt_text": tgt_text,
+            "encoder_token_len":len(encoder_input),
+            "decoder_token_len":len(decoder_input),
+            "pad_token": self.pad_token
+        })
+
+def casual_mask(size):
+    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+    return(mask == 0)
